942043ee86e6800eb1a82b305f9a6c17df6ab965
ashkett <ketkar.ashwin@gmail.com>
2025-02-08 12:00:08 +0530

    frontend name changes

--- backend/models/appointmentModel.js
+++ backend/models/appointmentModel.js
@@ -11,7 +11,13 @@ const appointmentSchema = new mongoose.Schema({
     date: { type: Number, required: true },
     cancelled: { type: Boolean, default: false },
     payment: { type: Boolean, default: false },
-    isCompleted: { type: Boolean, default: false }
+    isCompleted: { type: Boolean, default: false },
+    urgency: { 
+        type: String,
+        enum: ['high', 'medium', 'low'],
+        default: 'medium', 
+        required: true
+    }
 })
 
 const appointmentModel = mongoose.models.appointment || mongoose.model("appointment", appointmentSchema)
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,8 +7,9 @@ const Footer = () => {
       <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10  mt-40 text-sm'>
 
         <div>
-          <img className='mb-5 w-40' src={assets.logo} alt="" />
-          <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+          {/* <img className='mb-5 w-40' src={assets.logo} alt="" /> */}
+          <h1>TeleMedX</h1>
+          <p className='w-full md:w-2/3 text-gray-600 leading-6'>footer</p>
         </div>
 
         <div>
@@ -24,17 +25,14 @@ const Footer = () => {
         <div>
           <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
           <ul className='flex flex-col gap-2 text-gray-600'>
-            <li>+1-212-456-7890</li>
-            <li>greatstackdev@gmail.com</li>
+            <li>+91 7620594657</li>
+            <li>ketkar.ashwin@gmail.com</li>
           </ul>
         </div>
 
       </div>
 
-      <div>
-        <hr />
-        <p className='py-5 text-sm text-center'>Copyright 2024 @ Prescripto.com - All Right Reserved.</p>
-      </div>
+     
 
     </div>
   )
